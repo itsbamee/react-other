554555bb816bbcb524f42e8d86cb772d9eb188e9
tubi55 <tubi55@nate.com>
2023-10-07 13:41:54 +0900

    NavLink로 메뉴활성화 처리

--- src/App.js
+++ src/App.js
@@ -13,14 +13,12 @@ function App() {
 	return (
 		<>
 			<Header />
-
 			<Route exact path='/' component={Visual} />
 			<Route path='/department' component={Department} />
 			<Route path='/gallery' component={Gallery} />
 			<Route path='/youtube' component={Youtube} />
 			<Route path='/members' component={Members} />
 			<Route path='/contact' component={Contact} />
-
 			<Footer />
 		</>
 	);
