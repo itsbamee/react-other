af85f74f7eaea00704633cf7a67105f8b427877f
tubi55 <tubi55@nate.com>
2023-10-07 14:44:24 +0900

    footer styling

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -2,7 +2,7 @@ import Layout from '../../common/layout/Layout';
 
 export default function Contact() {
 	return (
-		<Layout title={'Contact Us'}>
+		<Layout title={'Contact us'}>
 			<p>찾아오는길 상세페이지</p>
 		</Layout>
 	);
