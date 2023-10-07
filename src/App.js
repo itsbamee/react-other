ad8b3657de181cb8f8bbf1bc1a0f5763237247d7
tubi55 <tubi55@nate.com>
2023-10-07 13:19:57 +0900

    router를 이용해서 url별로 컴포넌트 페이지 분리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import Footer from './components/common/footer/Footer';
 import Header from './components/common/header/Header';
 import Visual from './components/main/visual/Visual';
+import Contact from './components/sub/contact/Contact';
+import Department from './components/sub/department/Department';
+import Gallery from './components/sub/gallery/Gallery';
+import Members from './components/sub/members/Members';
+import Youtube from './components/sub/youtube/Youtube';
 import './styles/Global.scss';
+import { Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Visual />
+
+			<Route exact path='/' component={Visual} />
+			<Route path='/department' component={Department} />
+			<Route path='/gallery' component={Gallery} />
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/members' component={Members} />
+			<Route path='/contact' component={Contact} />
+
+			<Footer />
 		</>
 	);
 }
