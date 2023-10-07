cb2a6b86ddab34362924e2ef7dc8106788155e34
tubi55 <tubi55@nate.com>
2023-10-07 12:54:47 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/common/header/Header';
+import Visual from './components/main/visual/Visual';
+import './styles/Global.scss';
+
+function App() {
+	return (
+		<>
+			<Header />
+			<Visual />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,17 @@
+import './Header.scss';
+
+export default function Header() {
+	return (
+		<header>
+			<h1>DCODELAB</h1>
+
+			<ul>
+				<li>Department</li>
+				<li>Gallery</li>
+				<li>Youtube</li>
+				<li>Members</li>
+				<li>Contact</li>
+			</ul>
+		</header>
+	);
+}
