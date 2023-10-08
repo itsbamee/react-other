391b395b2689284d7cb1b5f2e8de516d9ebd21da
tubi55 <tubi55@nate.com>
2023-10-08 14:50:00 +0900

    서브페이지 텍스트 모션 처리

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from 'react';
 
 export default function Layout({ children, title }) {
 	let newClass = title.toLowerCase().split(' ').join('_');
+	const refFrame = useRef(null);
 	const refTitle = useRef(null);
 
 	useEffect(() => {
@@ -17,10 +18,14 @@ export default function Layout({ children, title }) {
 			count++;
 		}
 		refTitle.current.innerHTML = tags;
+
+		setTimeout(() => {
+			refFrame.current.classList.add('on');
+		}, 300);
 	}, []);
 
 	return (
-		<section className={`layout ${newClass}`}>
+		<section ref={refFrame} className={`layout ${newClass}`}>
 			<h1 ref={refTitle}>{title}</h1>
 			{children}
 		</section>
