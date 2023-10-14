d7ca99494fb62e4f8c7545a45a68ea1bbc1b6a53
tubi55 <tubi55@nate.com>
2023-10-14 15:31:12 +0900

    youtube 영상 상세페이지에 출력

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,6 +1,7 @@
 import { useFetch } from '../../../hooks/useFetch';
 import Layout from '../../common/layout/Layout';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Youtube() {
 	const [Vids, setVids] = useState([]);
@@ -30,7 +31,10 @@ export default function Youtube() {
 						<h2>{data.snippet.title}</h2>
 						<p>{data.snippet.description}</p>
 						<div className='pic'>
-							<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+							{/* 썸네일 링크 클리시 url로 detail/고유 유튜브데이터 id */}
+							<Link to={`/detail/${data.id}`}>
+								<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+							</Link>
 						</div>
 					</article>
 				);
