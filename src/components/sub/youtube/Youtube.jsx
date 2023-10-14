e098a49f172f692bffb48d1ec6f4c7525d592718
tubi55 <tubi55@nate.com>
2023-10-14 16:03:52 +0900

    useReftch hook 구조 변경

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -5,22 +5,22 @@ import { Link } from 'react-router-dom';
 
 export default function Youtube() {
 	const [Vids, setVids] = useState([]);
-	//const fetchData = useFetch();
-	console.log(Vids);
+	const fetchData = useFetch();
+	//console.log(Vids);
 
 	useEffect(() => {
 		const api_key = 'AIzaSyDC60bIIkAJFzy7ji4a0Eo3AX6tYudhe1w';
 		const pid = 'PLYOPkdUKSFgWqafuDQN9di3uLJoTV3L3W';
 		const num = 10;
 		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
-		//fetchData(baseURL, setVids);
+		fetchData(baseURL, setVids);
 
-		fetch(baseURL)
-			.then((data) => data.json())
-			.then((json) => {
-				console.log(json.items);
-				setVids(json.items);
-			});
+		// fetch(baseURL)
+		// 	.then((data) => data.json())
+		// 	.then((json) => {
+		// 		console.log(json.items);
+		// 		setVids(json.items);
+		// 	});
 	}, []);
 
 	return (
@@ -31,7 +31,6 @@ export default function Youtube() {
 						<h2>{data.snippet.title}</h2>
 						<p>{data.snippet.description}</p>
 						<div className='pic'>
-							{/* 썸네일 링크 클리시 url로 detail/고유 유튜브데이터 id */}
 							<Link to={`/detail/${data.id}`}>
 								<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
 							</Link>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,13 +3,12 @@ export function useFetch() {
 		fetch(url)
 			.then((data) => data.json())
 			.then((json) => {
-				const arr = Object.values(json).filter((data) => data.length);
-				console.log(arr);
-
-				const result = arr.filter((data) => {
-					if (data.length) return data;
-				});
-				//setValue(result);
+				//json객체의 property가 여러개일떄 value값만 반복돌면 value의 값이 배열인것만 리턴
+				let arr = Object.values(json).filter((data) => Array.isArray(data));
+				//혹시 리턴된 배열안에 또 배열이 있다면 해당 값을 다시 뽑아냄
+				if (Array.isArray(arr)) arr = arr[0];
+				//배열값을 state변경함수로 등록
+				setValue(arr);
 				setKey && setKey(Object.keys(json)[0]);
 			});
 	};
