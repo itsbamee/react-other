38ba1483e40a370dacff6989dbc8e6fee354ab59
tubi55 <tubi55@nate.com>
2023-10-22 12:50:36 +0900

    mansory 컴포넌트 적용

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,5 +1,6 @@
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
+import Masonry from 'react-masonry-component';
 import { useState, useEffect } from 'react';
 
 export default function Gallery() {
@@ -23,27 +24,36 @@ export default function Gallery() {
 	return (
 		<Layout title={'Gallery'}>
 			<div className='frame'>
-				{Pics.map((pic, idx) => {
-					return (
-						<article key={idx}>
-							<div className='pic'>
-								<img
-									src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-									alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
-								/>
-							</div>
-							<h2>{pic.title}</h2>
+				<Masonry
+					elementType={'div'}
+					options={{ transitionDuration: 0.5 }}
+					disableImagesLoaded={false}
+					updateOnEachImageLoad={false}
+				>
+					{Pics.map((pic, idx) => {
+						return (
+							<article key={idx}>
+								<div className='inner'>
+									<div className='pic'>
+										<img
+											src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+											alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+										/>
+									</div>
+									<h2>{pic.title}</h2>
 
-							<div className='profile'>
-								<img
-									src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
-									alt={pic.owner}
-								/>
-								<span>{pic.owner}</span>
-							</div>
-						</article>
-					);
-				})}
+									<div className='profile'>
+										<img
+											src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+											alt={pic.owner}
+										/>
+										<span>{pic.owner}</span>
+									</div>
+								</div>
+							</article>
+						);
+					})}
+				</Masonry>
 			</div>
 		</Layout>
 	);
