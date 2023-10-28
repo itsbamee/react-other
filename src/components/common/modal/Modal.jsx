37a08ee1b6403c1b87f3de875948adf85ef2b73c
tubi55 <tubi55@nate.com>
2023-10-28 14:58:09 +0900

    기본 framer모션 모션 처리

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -1,5 +1,11 @@
 import './Modal.scss';
 import { useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+/*
+  motion: 모션을 걸고 싶은 JSX요소 앞쪽에 motion.를 추가하면 initial, animate, exit라는 속성으로 모션설정 가능케하는 컴포넌트
+  AnimatePresence: 모션을 적용할 컴포넌트의 Wrapping 컴포넌트 - 자식요소의 모션이 끝날때까지 언마운트 되는 시점을 holding처리
+  적용가능한 모션 속성 : opacity, scale, rotate, x, y
+*/
 
 export default function Modal({ IsOpen, setIsOpen, children }) {
 	useEffect(() => {
@@ -7,13 +13,19 @@ export default function Modal({ IsOpen, setIsOpen, children }) {
 	}, [IsOpen]);
 
 	return (
-		<>
+		<AnimatePresence>
 			{IsOpen && (
-				<aside className='modal'>
+				<motion.aside
+					className='modal'
+					initial={{ opacity: 0, x: '100%' }} //JSX가 마운트되기 전상태의 스타일
+					animate={{ opacity: 1, x: '0%' }} //JSX가 마운트된 후의 스타일
+					exit={{ opacity: 0, x: '-100%' }} //JSX가 앞으로 언마운트될때의 스타일
+					transition={{ duration: 1 }} //스타일이 변경될떄의 전환시간
+				>
 					<div className='con'>{children}</div>
 					<span onClick={() => setIsOpen(false)}>close</span>
-				</aside>
+				</motion.aside>
 			)}
-		</>
+		</AnimatePresence>
 	);
 }
