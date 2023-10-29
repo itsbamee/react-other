87bb35f04c7a2198be1e60b4aceb56951532ddaf
tubi55 <tubi55@nate.com>
2023-10-29 14:37:27 +0900

    로컬저장소에 데이터 저장 및 화면 렌더링 순서 흐름

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -5,9 +5,16 @@ import { RxReset } from 'react-icons/rx';
 import { useRef, useState, useEffect } from 'react';
 
 function Comunity() {
+	//순서1- 로컬저장소의 값을 가져와서 객체화한다음 리턴하는 함수
+	const getLocalData = () => {
+		const data = localStorage.getItem('posts');
+		return JSON.parse(data);
+	};
+
 	const refInput = useRef(null);
 	const refTextarea = useRef(null);
-	const [Posts, setPosts] = useState([]);
+	//순서2-컴포넌트가 마운트되자마다 로컬저장소에서 가져온 배열값을 Posts state에 옮겨담음
+	const [Posts, setPosts] = useState(getLocalData());
 	console.log(Posts);
 
 	const resetPost = () => {
@@ -25,6 +32,7 @@ function Comunity() {
 	};
 
 	useEffect(() => {
+		//순서5- Posts값이 변경될떄마다 해당값을 문자화해서 로컬저장소에 저장
 		localStorage.setItem('posts', JSON.stringify(Posts));
 	}, [Posts]);
 
@@ -39,6 +47,7 @@ function Comunity() {
 						<button onClick={resetPost}>
 							<RxReset fontSize={20} color={'#555'} />
 						</button>
+						{/* 순서4- 글작성시 State값 변경처리 */}
 						<button onClick={createPost}>
 							<TfiWrite fontSize={20} color={'#555'} />
 						</button>
@@ -46,6 +55,7 @@ function Comunity() {
 				</div>
 
 				<div className='showBox'>
+					{/* 순서3- 로컬저장소로부터 옮겨담아진 state값을 반복돌면서 글 목록 출력 */}
 					{Posts.map((post, idx) => (
 						<article key={idx}>
 							<div className='txt'>
