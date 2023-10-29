aa44abbc56f69f76967d9e50791284833d243e19
tubi55 <tubi55@nate.com>
2023-10-29 13:04:41 +0900

    레이아웃 구조 변경

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -10,18 +10,22 @@ function Comunity() {
 
 	return (
 		<Layout title={'Community'}>
-			<div className='inputBox'>
-				<input type='text' placeholder='title' ref={refInput} />
-				<textarea cols='30' rows='3' placeholder='leave message' ref={refTextarea}></textarea>
+			<div className='wrap'>
+				<div className='inputBox'>
+					<input type='text' placeholder='title' ref={refInput} />
+					<textarea cols='30' rows='3' placeholder='leave message' ref={refTextarea}></textarea>
 
-				<nav>
-					<button>
-						<ImCancelCircle fontSize={20} color={'#555'} />
-					</button>
-					<button>
-						<TfiWrite fontSize={20} color={'#555'} />
-					</button>
-				</nav>
+					<nav>
+						<button>
+							<ImCancelCircle fontSize={20} color={'#555'} />
+						</button>
+						<button>
+							<TfiWrite fontSize={20} color={'#555'} />
+						</button>
+					</nav>
+				</div>
+
+				<div className='showBox'></div>
 			</div>
 		</Layout>
 	);
