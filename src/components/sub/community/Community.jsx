9c68a635c19c0189b007ec7353f18c23d3798bba
tubi55 <tubi55@nate.com>
2023-10-29 13:50:21 +0900

    글저장, 글호출 완료

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -2,11 +2,27 @@ import './Community.scss';
 import Layout from '../../common/layout/Layout';
 import { TfiWrite } from 'react-icons/tfi';
 import { RxReset } from 'react-icons/rx';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 function Comunity() {
 	const refInput = useRef(null);
 	const refTextarea = useRef(null);
+	const [Posts, setPosts] = useState([]);
+	console.log(Posts);
+
+	const resetPost = () => {
+		refInput.current.value = '';
+		refTextarea.current.value = '';
+	};
+
+	const createPost = () => {
+		if (!refInput.current.value.trim() || !refTextarea.current.value.trim()) {
+			resetPost();
+			return alert('제목과 본문을 모두 입력하세요.');
+		}
+		setPosts([{ title: refInput.current.value, content: refTextarea.current.value }, ...Posts]);
+		resetPost();
+	};
 
 	return (
 		<Layout title={'Community'}>
@@ -16,16 +32,23 @@ function Comunity() {
 					<textarea cols='30' rows='5' placeholder='Write Content Message' ref={refTextarea}></textarea>
 
 					<nav>
-						<button>
+						<button onClick={resetPost}>
 							<RxReset fontSize={20} color={'#555'} />
 						</button>
-						<button>
+						<button onClick={createPost}>
 							<TfiWrite fontSize={20} color={'#555'} />
 						</button>
 					</nav>
 				</div>
 
-				<div className='showBox'></div>
+				<div className='showBox'>
+					{Posts.map((post, idx) => (
+						<article key={idx}>
+							<h2>{post.title}</h2>
+							<p>{post.content}</p>
+						</article>
+					))}
+				</div>
 			</div>
 		</Layout>
 	);
