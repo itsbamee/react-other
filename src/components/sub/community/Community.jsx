350354e66d60f4785ac438c085e4c9917c9cf2db
tubi55 <tubi55@nate.com>
2023-10-29 13:21:02 +0900

    crud 주석 정리

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -12,8 +12,8 @@ function Comunity() {
 		<Layout title={'Community'}>
 			<div className='wrap'>
 				<div className='inputBox'>
-					<input type='text' placeholder='title' ref={refInput} />
-					<textarea cols='30' rows='3' placeholder='leave message' ref={refTextarea}></textarea>
+					<input type='text' placeholder='Write Title' ref={refInput} />
+					<textarea cols='30' rows='3' placeholder='Write Content Message' ref={refTextarea}></textarea>
 
 					<nav>
 						<button>
@@ -32,3 +32,18 @@ function Comunity() {
 }
 
 export default Comunity;
+
+/*
+	Create (글작성) "POST"
+	Read (글 불러오기) "GET"
+	Update (글 수정) "PUT"
+	Delete (글 삭제) "DELETE"
+
+	RESTful API : DB의 구조적으로 변경하기위한 개발 방법론
+
+	로컬저장소: LocalStorage
+	- 모든 브라우저가 내장하고 있는 경량의 저장공간
+	- 문자값만 저장가능 (5MB)
+	- 객체값을 문자화시켜서 저장
+	- 로컬저장소 값을 불러올때는 반대로 문자형태를 JSON형태로 객체로 parsing해서 가져옴
+*/
