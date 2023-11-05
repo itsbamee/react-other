fb0b69365de1b7baa255530515732ce3ee07b6d9
tubi55 <tubi55@nate.com>
2023-11-05 14:56:35 +0900

    form요소에 변경점이 생길때마다 실시간으로 State값 변경

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -21,16 +21,18 @@ export default function Members() {
 	//만약 실시간으로 바뀌는 값을 무조건 value props로 연결하고 onChange이벤트 연결
 	//바뀌지 않는 정적인 값을 연결시에는 defaultValue props로 연결하고 onChange 이벤트 연결 불필요
 
-	//onChange이벤트가 발생할때마다 해당 함수 호출
 	const handleChange = (e) => {
-		//현재 입력하고 있는 가상돔요소의 name, value값을 비구조화할당으로 뽑아냄
 		const { name, value } = e.target;
-		//객체안에서 property key값을 []로 감싸면 변수로 치환가능
-		//name='userid'인 input요소의 onChange이벤트가 발생하면
-		//[name]--> 'userid', value: 내가 현재 입력하고 있는 값 등록
-		//handleChange가 연결된 폼에 특정 값을 입력할때마다 실시간으로 해당 name값에 매칭되는 객체 property가 변경되고 변경된 값으로 State수정
-		//State가 변경될때마다 컴포넌트 재호출되면서 Input요소의 value속성으로 현재 State값이 실시간으로 출력됨
 		setVal({ ...Val, [name]: value });
+	};
+
+	const handleCheck = (e) => {
+		const { name } = e.target;
+		let checkArr = [];
+		const inputs = e.target.parentElement.querySelectorAll('input');
+
+		inputs.forEach((input) => input.checked && checkArr.push(input.value));
+		setVal({ ...Val, [name]: checkArr });
 	};
 
 	useEffect(() => {
@@ -49,6 +51,7 @@ export default function Members() {
 							<legend className='h'>회원가입 폼</legend>
 							<table>
 								<tbody>
+									{/* userid, email (handleChange) */}
 									<tr>
 										<td>
 											<input
@@ -63,6 +66,8 @@ export default function Members() {
 											<input type='text' name='email' placeholder='Email' value={Val.email} onChange={handleChange} />
 										</td>
 									</tr>
+
+									{/* pwd1, pwd2 (handleChange) */}
 									<tr>
 										<td>
 											<input
@@ -84,9 +89,10 @@ export default function Members() {
 										</td>
 									</tr>
 
+									{/* edu (handleChange) */}
 									<tr>
 										<td colSpan='2'>
-											<select name='edu'>
+											<select name='edu' onChange={handleChange}>
 												<option defaultValue=''>Education</option>
 												<option defaultValue='elementary-school'>초등학교 졸업</option>
 												<option defaultValue='middle-school'>중학교 졸업</option>
@@ -95,30 +101,42 @@ export default function Members() {
 											</select>
 										</td>
 									</tr>
+
+									{/* gender (handleChange) */}
 									<tr>
 										<td colSpan='2'>
-											<input type='radio' defaultValue='female' id='female' name='gender' />
+											<input type='radio' defaultValue='female' id='female' name='gender' onChange={handleChange} />
 											<label htmlFor='female'>Female</label>
 
-											<input type='radio' defaultValue='male' id='male' name='gender' />
+											<input type='radio' defaultValue='male' id='male' name='gender' onChange={handleChange} />
 											<label htmlFor='male'>Male</label>
 										</td>
 									</tr>
+
+									{/* interests (handleCheck) */}
 									<tr>
 										<td colSpan='2'>
-											<input type='checkbox' name='interest' id='sports' defaultValue='sports' />
+											<input type='checkbox' name='interest' id='sports' defaultValue='sports' onChange={handleCheck} />
 											<label htmlFor='sports'>Sports</label>
 
-											<input type='checkbox' name='interest' id='reading' defaultValue='reading' />
+											<input
+												type='checkbox'
+												name='interest'
+												id='reading'
+												defaultValue='reading'
+												onChange={handleCheck}
+											/>
 											<label htmlFor='reading'>Reading</label>
 
-											<input type='checkbox' name='interest' id='music' defaultValue='music' />
+											<input type='checkbox' name='interest' id='music' defaultValue='music' onChange={handleCheck} />
 											<label htmlFor='music'>Music</label>
 
-											<input type='checkbox' name='interest' id='game' defaultValue='game' />
+											<input type='checkbox' name='interest' id='game' defaultValue='game' onChange={handleCheck} />
 											<label htmlFor='game'>Game</label>
 										</td>
 									</tr>
+
+									{/* comments (handleChange) */}
 									<tr>
 										<td colSpan='2'>
 											<textarea
