e5c6489e6ec2d61490452de015225cf1d7aa92d3
tubi55 <tubi55@nate.com>
2023-11-11 14:09:38 +0900

    마커이미지의 가로, 세로 위치값 보정

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -15,10 +15,10 @@ export default function Contact() {
 	};
 
 	//마커이미지 인스턴스를 생성하기 위한 정보값들
-	const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png';
-	const imageSize = new kakao.maps.Size(64, 69);
-	const imageOption = { offset: new kakao.maps.Point(27, 69) };
-	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+	const imgSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
+	const imgSize = new kakao.maps.Size(232, 99);
+	const imgPos = { offset: new kakao.maps.Point(116, 99) };
+	const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgPos);
 
 	const marker = new kakao.maps.Marker({
 		position: mapOption.center,
