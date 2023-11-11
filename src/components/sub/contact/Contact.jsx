3bf1e369bc53bbea34d294ff1d31b62cead2c6fb
tubi55 <tubi55@nate.com>
2023-11-11 14:19:38 +0900

    각 객체정보를 배열로 묶어두고 배열의 순번에 따라 지도화면 렌더링되도록 코드 수정

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -6,22 +6,37 @@ export default function Contact() {
 	const { kakao } = window;
 	const mapFrame = useRef(null);
 
-	const info = [
+	const info = useRef([
 		{
-			latlng: new kakao.maps.LatLng(37.51272797391666, 127.06076771220832),
+			title: '삼성역 코엑스',
+			latlng: new kakao.maps.LatLng(37.51100661425726, 127.06162026853143),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
 			imgSize: new kakao.maps.Size(232, 99),
 			imgPos: { offset: new kakao.maps.Point(116, 99) },
 		},
-	];
+		{
+			title: '넥슨 본사',
+			latlng: new kakao.maps.LatLng(37.40211707077346, 127.10344953763003),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker2.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+		{
+			title: '서울 시청',
+			latlng: new kakao.maps.LatLng(37.5662952, 126.9779451),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+	]);
 
 	const marker = new kakao.maps.Marker({
-		position: info[0].latlang,
-		image: new kakao.maps.MarkerImage(info[0].imgSrc, info[0].imgSize, info[0].imgPos),
+		position: info.current[1].latlng,
+		image: new kakao.maps.MarkerImage(info.current[1].imgSrc, info.current[1].imgSize, info.current[1].imgPos),
 	});
 
 	useEffect(() => {
-		const map = new kakao.maps.Map(mapFrame.current, { center: info[0].latlng });
+		const map = new kakao.maps.Map(mapFrame.current, { center: info.current[1].latlng });
 		marker.setMap(map);
 	}, []);
 
