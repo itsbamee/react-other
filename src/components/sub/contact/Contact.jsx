4e97630a6347f35640533749cbb5647c3abc4b26
tubi55 <tubi55@nate.com>
2023-11-11 14:01:25 +0900

    마커이미지 변경

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -13,8 +13,16 @@ export default function Contact() {
 		center: new kakao.maps.LatLng(37.51272797391666, 127.06076771220832),
 		level: 3,
 	};
+
+	//마커이미지 인스턴스를 생성하기 위한 정보값들
+	const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png';
+	const imageSize = new kakao.maps.Size(64, 69);
+	const imageOption = { offset: new kakao.maps.Point(27, 69) };
+	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
 	const marker = new kakao.maps.Marker({
 		position: mapOption.center,
+		image: markerImage,
 	});
 
 	useEffect(() => {
