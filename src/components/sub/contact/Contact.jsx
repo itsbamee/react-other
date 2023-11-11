137749a8037eb8c389364076faa2f58b1ef0b409
tubi55 <tubi55@nate.com>
2023-11-11 13:27:52 +0900

    기본 지도 인스턴스 생성

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,9 +1,26 @@
+import { useRef, useEffect } from 'react';
 import Layout from '../../common/layout/Layout';
+import './Contact.scss';
 
 export default function Contact() {
+	//JSX컴포넌트에 에서는 cdn을통해 window전역객체에 받아지는 kakao객체에 자동 접근이 안되니
+	//비구조화할당으로 직접 해당 객체 추출
+	const { kakao } = window;
+	//api적용할 요소도 가상돔이기 때문에 참조객체에 연결
+	const mapFrame = useRef(null);
+	const mapOption = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		level: 3,
+	};
+
+	useEffect(() => {
+		//인스턴스 복사는 컴포넌트 마운트시 처리
+		new kakao.maps.Map(mapFrame.current, mapOption);
+	}, []);
+
 	return (
 		<Layout title={'Contact us'}>
-			<p>찾아오는길 상세페이지</p>
+			<article id='map' ref={mapFrame}></article>
 		</Layout>
 	);
 }
