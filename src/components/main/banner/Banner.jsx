37d08260405c252c51503e2a5d2f1bbfb31c3b45
tubi55 <tubi55@nate.com>
2023-11-19 13:21:50 +0900

    Banner컴포넌트에 스크롤갑 연동

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -1,9 +1,26 @@
 import './Banner.scss';
+import { useRef, useEffect } from 'react';
 
 export default function Banner() {
+	console.log('Banner Called');
+	const currentEl = useRef(null);
+	const titleEl = useRef(null);
+
+	const handleScroll = () => {
+		const scroll = window.scrollY;
+		const modifiedScroll = scroll - currentEl.current?.offsetTop;
+		console.log(modifiedScroll);
+		titleEl.current.style.transform = `translateX(${modifiedScroll}px)`;
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+
 	return (
-		<section className='banner myScroll'>
-			<h1>Banner</h1>
+		<section className='banner myScroll' ref={currentEl}>
+			<h1 ref={titleEl}>Banner</h1>
 		</section>
 	);
 }
--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -3,19 +3,15 @@ import { useRef, useEffect, useState } from 'react';
 import Anime from '../../../asset/anime.js';
 
 function Btns() {
-	//section의 전체 갯수가 담길 값을 참조객체에서 state로 변경
-	//Num값 변경시 컴포넌트가 재랜더링되어야 버튼이 생기므로 state처리
 	const [Num, setNum] = useState(0);
 	const secs = useRef(null);
 	const btns = useRef(null);
 
-	//컴포넌트 마운트시 윈도우 스크롤이벤트에 연결될 함수
 	const activation = () => {
 		const scroll = window.scrollY;
 		secs.current.forEach((el, idx) => {
 			if (scroll >= el.offsetTop - window.innerHeight / 2) {
 				Array.from(btns.current.children).forEach((btn) => btn.classList.remove('on'));
-				//btns의 li요소가 동적으로 생성되기 전에 호출시 오류를 피하기 위해서 optional chaining처리
 				btns.current.children[idx]?.classList.add('on');
 
 				secs.current.forEach((sec) => sec.classList.remove('on'));
@@ -28,13 +24,9 @@ function Btns() {
 		new Anime(window, { scroll: secs.current[idx].offsetTop }, { duration: 500 });
 	};
 
-	//컴포넌트 마운트시
 	useEffect(() => {
-		//빈 참조객체에 버튼과 section요소 담아줌
 		secs.current = document.querySelectorAll('.myScroll');
 		setNum(secs.current.length);
-
-		//window scroll이벤트에 activation함수 연결
 		window.addEventListener('scroll', activation);
 
 		return () => {
@@ -42,11 +34,7 @@ function Btns() {
 		};
 	}, []);
 
-	//Num state변경시 activation호출
-	useEffect(() => {
-		//마운트시 section의 첫번째 요소에 on을 붙여주기 위함
-		activation();
-	}, [Num]);
+	useEffect(activation, [Num]);
 
 	return (
 		<ul className='btns' ref={btns}>
