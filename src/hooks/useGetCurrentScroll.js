c100754d3ba60982d2831ff9defecc552da18d92
tubi55 <tubi55@nate.com>
2023-11-19 14:19:06 +0900

    스크롤 커스텀훅에 공통적으로 baseLine값 추가 및 적용

--- src/hooks/useGetCurrentScroll.js
+++ src/hooks/useGetCurrentScroll.js
@@ -1,10 +1,11 @@
 export function useGetCurrentScroll() {
 	return (refEl) => {
 		const scroll = window.scrollY;
+		const baseLine = window.innerHeight / 2;
 		let customScroll = 0;
 
-		if (scroll >= refEl.current?.offsetTop) {
-			customScroll = scroll - refEl.current?.offsetTop;
+		if (scroll >= refEl.current?.offsetTop - baseLine) {
+			customScroll = scroll - refEl.current?.offsetTop + baseLine;
 		} else {
 			customScroll = 0;
 		}
