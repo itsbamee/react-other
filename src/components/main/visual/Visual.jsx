a814e545bdca4780a2970ca809f5621410e77980
tubi55 <tubi55@nate.com>
2023-11-26 13:46:07 +0900

    기본 swiper 적용

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,5 +1,16 @@
 import './Visual.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 export default function Visual() {
-	return <figure className='myScroll'>Visual</figure>;
+	return (
+		<figure className='myScroll'>
+			<Swiper spaceBetween={50} slidesPerView={3}>
+				<SwiperSlide>Slide 1</SwiperSlide>
+				<SwiperSlide>Slide 2</SwiperSlide>
+				<SwiperSlide>Slide 3</SwiperSlide>
+				<SwiperSlide>Slide 4</SwiperSlide>
+			</Swiper>
+		</figure>
+	);
 }
