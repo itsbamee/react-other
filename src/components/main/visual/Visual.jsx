420e77f8dcefd9b1fa09e62bbc9e184c0201aeb7
tubi55 <tubi55@nate.com>
2023-11-26 14:33:01 +0900

    useRef를 통한 정적인 값 담아주기

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,7 +1,7 @@
 import './Visual.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 //0. fetching될 데이터가 담길 State생성
 //1. DB폴더의 데이터를 fetching한뒤 state에 담는함수 추가 (async await)
@@ -10,9 +10,12 @@ import { useEffect, useState } from 'react';
 
 export default function Visual() {
 	const [SlideData, setSlideData] = useState([]);
+	//바뀌지 않는 정적인 값을 담을때에는 가급적 참조객체에 담아줌
+	//일반변수로 담으면 다시 useMemo를 통해서 메모이제이션해야 되는 번거로움을 피하기 위함
+	const path = useRef(process.env.PUBLIC_URL);
 
 	const fetchData = async () => {
-		const data = await fetch(process.env.PUBLIC_URL + '/DB/department.json');
+		const data = await fetch(`${path.current}/DB/department.json`);
 		const json = await data.json();
 		console.log(json.members);
 		setSlideData(json.members);
@@ -29,7 +32,7 @@ export default function Visual() {
 					return (
 						<SwiperSlide key={idx}>
 							<div className='pic'>
-								<img src={process.env.PUBLIC_URL + '/img/' + data.pic} alt={data.name} />
+								<img src={`${path.current}/img/${data.pic}`} alt={data.name} />
 							</div>
 						</SwiperSlide>
 					);
