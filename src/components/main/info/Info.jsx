d0e7bca89811f5c72b6f82dd33ad96eb30e90bd0
tubi55 <tubi55@nate.com>
2023-11-26 12:53:12 +0900

    Info.jsx안쪽의 handleClick함수 메모이제이션 처리

--- src/components/main/info/Info.jsx
+++ src/components/main/info/Info.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useCallback } from 'react';
 import './Info.scss';
 import { useGetCurrentScroll } from '../../../hooks/useGetCurrentScroll';
 
@@ -7,16 +7,16 @@ function Info() {
 	const boxEl = useRef(null);
 	const getScroll = useGetCurrentScroll();
 
-	const handleClick = () => {
+	const handleClick = useCallback(() => {
 		const modifiedScroll = getScroll(currentEl);
 		boxEl.current.style.transform = `rotate(${modifiedScroll}deg) scale(${1 + modifiedScroll / 300})`;
 		boxEl.current.style.opacity = 1 - modifiedScroll / 600;
-	};
+	}, [getScroll]);
 
 	useEffect(() => {
 		window.addEventListener('scroll', handleClick);
 		return () => window.removeEventListener('scroll', handleClick);
-	}, []);
+	}, [handleClick]);
 
 	return (
 		<section className='info myScroll' ref={currentEl}>
