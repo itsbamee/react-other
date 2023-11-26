7636019d75a7272087d4c9842ce4d6c22a2f6e63
tubi55 <tubi55@nate.com>
2023-11-26 12:42:50 +0900

    특정 핸들러함수를 useCallback으로 메모이제이션처리해서 무한루프 해결

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -1,21 +1,25 @@
 import { useGetCurrentScroll } from '../../../hooks/useGetCurrentScroll';
 import './Banner.scss';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useCallback } from 'react';
 
 export default function Banner() {
 	const currentEl = useRef(null);
 	const titleEl = useRef(null);
 	const getScroll = useGetCurrentScroll();
 
-	const handleScroll = () => {
+	//hadleScroll에 대입되어 있는 함수가 컴포넌트 재랜더링시마다 계속 읽히기 때문에
+	//useCallback을 통해서 강제로 메모리에 등록해서 기존함수내용을 재활용하는 메모이제이션 처리
+	//메모이제이션되는 순간 그안쪽의 모든 값들이 static하게 고정되어 버리므로
+	//의존성배열에 특정 값을 지정해서 해당 값이 변경시에는 임시로 메모이제이션 해제
+	const handleScroll = useCallback(() => {
 		const modifiedScroll = getScroll(currentEl);
 		titleEl.current.style.transform = `translateX(${modifiedScroll}px)`;
-	};
+	}, [getScroll]);
 
 	useEffect(() => {
 		window.addEventListener('scroll', handleScroll);
 		return () => window.removeEventListener('scroll', handleScroll);
-	}, []);
+	}, [handleScroll]);
 
 	return (
 		<section className='banner myScroll' ref={currentEl}>
