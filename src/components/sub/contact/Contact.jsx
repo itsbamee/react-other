3178b374e8684596f5e40d4fbd7c4a84e1f013bc
tubi55 <tubi55@nate.com>
2023-11-26 13:07:22 +0900

    useMemo와 useCallback을 통한 메모이제이션

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState, useCallback, useMemo } from 'react';
 import Layout from '../../common/layout/Layout';
 import emailjs from '@emailjs/browser';
 import './Contact.scss';
@@ -38,22 +38,21 @@ export default function Contact() {
 		},
 	]);
 
-	const marker = new kakao.maps.Marker({
-		position: info.current[Index].latlng,
-		image: new kakao.maps.MarkerImage(
-			info.current[Index].imgSrc,
-			info.current[Index].imgSize,
-			info.current[Index].imgPos
-		),
-	});
+	//useMemo는 함수자체가 아닌 함수가 반환하는 리턴값을 메모이제이션처리하기 때문에
+	//무조건 첫번째인수값으로 특정값을 리턴하는 익명함수가 등록되어야함
+	const marker = useMemo(() => {
+		return new kakao.maps.Marker({
+			position: info.current[Index].latlng,
+			image: new kakao.maps.MarkerImage(info.current[Index].imgSrc, info.current[Index].imgSize, info.current[Index].imgPos),
+		});
+	}, [kakao, Index]);
 
 	const sendEmail = (e) => {
 		e.preventDefault();
 		const [user, email] = form.current.querySelectorAll('input');
 		const txtArea = form.current.querySelector('textarea');
 
-		if (!user.value || !email.value || !txtArea.value)
-			return alert('이름, 답장받을 메일주소, 문의내용을 모두 입력하세요.');
+		if (!user.value || !email.value || !txtArea.value) return alert('이름, 답장받을 메일주소, 문의내용을 모두 입력하세요.');
 
 		emailjs.sendForm('service_blebqd2', 'template_93zpfge', form.current, '23g8RepczesqKPoIX').then(
 			(result) => {
@@ -68,18 +67,18 @@ export default function Contact() {
 		);
 	};
 
-	const roadView = () => {
+	const roadView = useCallback(() => {
 		//roadview setting
 		//두번째 인수값 50(m단위)은 마커위치로부터 로드뷰가 출력될수 있는 가장 가까운 거리의 범위지정
 		new kakao.maps.RoadviewClient().getNearestPanoId(info.current[Index].latlng, 50, (id) => {
 			new kakao.maps.Roadview(viewFrame.current).setPanoId(id, info.current[Index].latlng);
 		});
-	};
+	}, [Index, kakao]);
 
-	const setCenter = () => {
+	const setCenter = useCallback(() => {
 		mapInstance.current.setCenter(info.current[Index].latlng);
 		roadView();
-	};
+	}, [Index, roadView]);
 
 	useEffect(() => {
 		mapFrame.current.innerHTML = '';
@@ -94,18 +93,18 @@ export default function Contact() {
 		roadView();
 
 		window.addEventListener('resize', setCenter);
-	}, [Index]);
+	}, [Index, kakao, marker, roadView, setCenter]);
 
 	//교통정보 보기 토글 기능
 	useEffect(() => {
 		Traffic
 			? mapInstance.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
 			: mapInstance.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-	}, [Traffic]);
+	}, [kakao, Traffic]);
 
 	useEffect(() => {
 		return () => window.removeEventListener('resize', setCenter);
-	}, []);
+	}, [setCenter]);
 
 	return (
 		<Layout title={'Contact us'}>
