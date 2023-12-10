858983da212fe01bc0601bc3591aa83c755e3e4d
tubi55 <tubi55@nate.com>
2023-12-10 14:04:17 +0900

    버튼 활성화 스타일링

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -107,7 +107,7 @@ function Comunity() {
 					.fill()
 					.map((_, idx) => {
 						return (
-							<button key={idx} onClick={() => setCurNum(idx)}>
+							<button key={idx} onClick={() => setCurNum(idx)} className={idx === CurNum ? 'on' : ''}>
 								{idx + 1}
 							</button>
 						);
