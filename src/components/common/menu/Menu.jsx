ea0ed0751e8808e6ef91ee3c6edeb053da8fdd9b
tubi55 <tubi55@nate.com>
2023-12-10 15:06:17 +0900

    모바일 패널 스타일링

--- src/components/common/menu/Menu.jsx
+++ src/components/common/menu/Menu.jsx
@@ -1,5 +1,6 @@
 import './Menu.scss';
 import { AnimatePresence, motion } from 'framer-motion';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Menu({ IsMenu, setIsMenu }) {
 	return (
@@ -13,7 +14,42 @@ export default function Menu({ IsMenu, setIsMenu }) {
 					exit={{ opacity: 0, x: '-100%' }}
 					transition={{ duration: 0.5 }}
 				>
-					Menu
+					<h1>
+						<Link to='/'>DCODELAB</Link>
+					</h1>
+
+					<ul>
+						<li>
+							<NavLink to='/department' activeClassName={'on'}>
+								Department
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/community' activeClassName={'on'}>
+								Community
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/gallery' activeClassName={'on'}>
+								Gallery
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/youtube' activeClassName={'on'}>
+								Youtube
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/members' activeClassName={'on'}>
+								Members
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/contact' activeClassName={'on'}>
+								Contact
+							</NavLink>
+						</li>
+					</ul>
 				</motion.aside>
 			)}
 		</AnimatePresence>
