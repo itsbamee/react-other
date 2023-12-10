0d8008c2f7099e571856ead916373b161bebdd2d
tubi55 <tubi55@nate.com>
2023-12-10 14:56:00 +0900

    메뉴 슬라이드 모션 추가

--- src/components/common/menu/Menu.jsx
+++ src/components/common/menu/Menu.jsx
@@ -1,11 +1,21 @@
 import './Menu.scss';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function Menu({ IsMenu, setIsMenu }) {
 	return (
-		IsMenu && (
-			<aside className='Menu' onClick={() => setIsMenu(false)}>
-				Menu
-			</aside>
-		)
+		<AnimatePresence>
+			{IsMenu && (
+				<motion.aside
+					className='Menu'
+					onClick={() => setIsMenu(false)}
+					initial={{ opacity: 0, x: '-100%' }}
+					animate={{ opacity: 1, x: '0%' }}
+					exit={{ opacity: 0, x: '-100%' }}
+					transition={{ duration: 0.5 }}
+				>
+					Menu
+				</motion.aside>
+			)}
+		</AnimatePresence>
 	);
 }
