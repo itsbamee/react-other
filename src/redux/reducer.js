121883046d991409c186975c51603679c973c9d0
tubi55 <tubi55@nate.com>
2023-12-16 13:59:23 +0900

    reducer 조건문 switch문으로 변경

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -35,8 +35,19 @@ const initMemberData = {
 	]
 };
 
-//초기 데이터를 전역 state에 저장하고 추후 action객체가 넘어오면 action의 type에 따라서 전역데이터를 변경해주는 변형자함수 생성: reducer
-//액션 {type: 'SET_MEMBERS', payload:[memberData]}
+const memberReducer = (state = initMemberData, action) => {
+	switch (action.type) {
+		case 'SET_MEMBERS':
+			return { ...state, members: action.payload };
+		default:
+			return state;
+	}
+};
+
+const reducers = combineReducers({ memberReducer });
+export default reducers;
+
+/*
 const memberReducer = (state = initMemberData, action) => {
 	if (action.type === 'SET_MEMBERS') {
 		return { ...state, members: action.payload };
@@ -44,7 +55,4 @@ const memberReducer = (state = initMemberData, action) => {
 		return state;
 	}
 };
-
-//해당 리듀서함수가 반환하는 객체를 외부로 export
-const reducers = combineReducers({ memberReducer });
-export default reducers;
+*/
