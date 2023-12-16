611b8350d3bbe792c2bcd67ad5ed48c4ec27dce1
tubi55 <tubi55@nate.com>
2023-12-16 14:19:18 +0900

    비동기 데이터 전역 스토어에 저장되고 활용되는 흐름 정리

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -4,6 +4,7 @@ import './Footer.scss';
 import { useSelector } from 'react-redux';
 
 export default function Footer() {
+	//순서4 - 전역 store로부터 데이터 전달 받음
 	const MemberData = useSelector(store => store.memberReducer.members);
 	console.log(MemberData);
 
@@ -12,6 +13,7 @@ export default function Footer() {
 			<h1>Dcodelab</h1>
 
 			<p>2023 Dcodelab &copy; All Rights Reserved.</p>
+			{/* 전역 state값 출력: 주의 첫번째 마운트시에는 빈배열값이므로 옵셔널 체이닝처리 */}
 			<p>This company was founced by {MemberData[0]?.name}</p>
 
 			<ul>
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 
+//순서1 - 리듀서함수가 실행되는 시점에는 전역에 담을 비동기 데이터가 없으므로 빈배열로 전역state 초기화
 const memberReducer = (state = { members: [] }, action) => {
 	switch (action.type) {
 		case 'SET_MEMBERS':
