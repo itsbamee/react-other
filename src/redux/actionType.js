f1664f78ef17ee3854ab9eb675ad3b048f328891
tubi55 <tubi55@nate.com>
2023-12-17 15:13:23 +0900

    action타입명을 효율적으로 관리하기 위해 actionType객체 생성

---
+++ src/redux/actionType.js
@@ -0,0 +1,11 @@
+export const FLICKR = {
+  start: 'FLICKR_START',
+  success: 'FLICKR_SUCCESS',
+  fail: 'FLICKR_FAIL',
+};
+
+export const YOUTUBE = {
+  start: 'YOUTUBE_START',
+  success: 'YOUTUBE_SUCCESS',
+  fail: 'YOUTUBE_FAIL',
+};
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,17 +1,18 @@
 import { combineReducers } from 'redux';
+import * as actionType from './actionType';
 
 const flickrReducer = (state = { flickr: [] }, action) => {
   switch (action.type) {
     //컴포넌트에서 dispatch로 전달되는 초기 action타입
     //해당 액션타입을 리듀서가 받자마자 해당 업무를 saga한테 전달
     //saga가 해당 액션타입을 전달받아서 데이터 fetching요청 시작 (success액션, fail액션 객체 생성)
-    case 'FLICKR_START':
+    case actionType.FLICKR.start:
       return state;
     //saga가 전달한 action객체 타입이 success일때 데이터 가공
-    case 'FLICKR_SUCCESS':
+    case actionType.FLICKR.success:
       return { ...state, flickr: action.payload };
     //saga가 전달한 action객체 타입이 fail일때 데이터 가공
-    case 'FLICKR_FAIL':
+    case actionType.FLICKR.fail:
       return { ...state, flickr: action.payload };
     //위 3가지 경우의 액션타입이 아니면 아무런 작업하지 않고 기존 전역객체 내보냄
     default:
