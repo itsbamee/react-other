523ef4084438b403b9537f44d2eff5cfdc9a798a
tubi55 <tubi55@nate.com>
2023-12-17 14:48:58 +0900

    saga용 리듀서함수 액션 타입 세분화에 따른 데이터변경 분기처리

---
+++ src/redux/reducer.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const flickrReducer = (state = { flickr: [] }, action) => {
+  switch (action.type) {
+    //컴포넌트에서 dispatch로 전달되는 초기 action타입
+    //해당 액션타입을 리듀서가 받자마자 해당 업무를 saga한테 전달
+    //saga가 해당 액션타입을 전달받아서 데이터 fetching요청 시작 (success액션, fail액션 객체 생성)
+    case 'FLICKR_START':
+      return state;
+    //saga가 전달한 action객체 타입이 success일때 데이터 가공
+    case 'FLICKR_SUCCESS':
+      return { ...state, flickr: action.payload };
+    //saga가 전달한 action객체 타입이 fail일때 데이터 가공
+    case 'FLICKR_FAIL':
+      return { ...state, flickr: action.payload };
+    //위 3가지 경우의 액션타입이 아니면 아무런 작업하지 않고 기존 전역객체 내보냄
+    default:
+      return state;
+  }
+};
+
+const reducers = combineReducers({ flickrReducer });
+export default reducers;
