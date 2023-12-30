381fb5c8c0f26015a7c131799ddbc4690d7737c4
itsbamee <jan9239@naver.com>
2023-12-30 15:00:14 +0900

    history, department데이터 react-query로 관리

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,65 +1,38 @@
 import './Department.scss';
 import Layout from '../../common/layout/Layout';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useMembersQuery } from '../../../hooks/useMemberQuery';
+import { useHistoryQuery } from '../../../hooks/useHistoryQuery';
 
 const path = process.env.PUBLIC_URL;
 
 export default function Department() {
-	const [Num, setNum] = useState(0);
-	const [History, setHistory] = useState([]);
-
-	//커스텀훅 호출시 인수로 데이터에서 뽑아낼 데이터의 순번을 전달
-	const { data } = useMembersQuery(Num);
-	console.log(data);
-
-	const fetchHistory = async () => {
-		const data = await fetch(`${path}/DB/history.json`);
-		const json = await data.json();
-
-		setHistory(json.history);
-	};
-
-	useEffect(() => {
-		fetchHistory();
-	}, []);
+	const {
+		isLoading: memberLoading,
+		isError: memberErr,
+		isSuccess: memberSuccess,
+		data: member,
+	} = useMembersQuery();
+	const { isSuccess: historySuccess, data: history } = useHistoryQuery();
 
 	return (
 		<Layout title={'Department'}>
-			<button onClick={() => setNum(0)}>데이터0 확인</button>
-			<button onClick={() => setNum(1)}>데이터1 확인</button>
 			<section id='historyBox'>
 				<h2>History</h2>
 				<div className='con'>
-					{History.map((data, idx) => {
-						return (
-							<React.Fragment key={idx}>
-								<h3>{Object.keys(data)[0]}</h3>
-								<ul>
-									{Object.values(data)[0].map((val, idx) => (
-										<li key={idx}>{val}</li>
-									))}
-								</ul>
-							</React.Fragment>
-						);
-					})}
-				</div>
-			</section>
-			{/* <section id='historyBox'>
-				<h2>History</h2>
-				<div className='con'>
-					{History.map((data, idx) => {
-						return (
-							<React.Fragment key={idx}>
-								<h3>{Object.keys(data)[0]}</h3>
-								<ul>
-									{Object.values(data)[0].map((val, idx) => (
-										<li key={idx}>{val}</li>
-									))}
-								</ul>
-							</React.Fragment>
-						);
-					})}
+					{historySuccess &&
+						history.map((data, idx) => {
+							return (
+								<React.Fragment key={idx}>
+									<h3>{Object.keys(data)[0]}</h3>
+									<ul>
+										{Object.values(data)[0].map((val, idx) => (
+											<li key={idx}>{val}</li>
+										))}
+									</ul>
+								</React.Fragment>
+							);
+						})}
 				</div>
 			</section>
 
@@ -67,9 +40,9 @@ export default function Department() {
 				<h2>Department</h2>
 
 				<div className='con'>
-					{isLoading && <p>Loading...</p>}
-					{isSuccess &&
-						Department.map((member, idx) => {
+					{memberLoading && <p>Loading...</p>}
+					{memberSuccess &&
+						member.map((member, idx) => {
 							return (
 								<article key={idx}>
 									<div className='pic'>
@@ -80,9 +53,10 @@ export default function Department() {
 								</article>
 							);
 						})}
-					{isError && <p>Fail to load Data</p>}
+
+					{memberErr && <p>Fail to load Data</p>}
 				</div>
-			</section> */}
+			</section>
 		</Layout>
 	);
 }
--- src/hooks/useMemberQuery.js
+++ src/hooks/useMemberQuery.js
@@ -1,21 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 
-const fetchMembers = async ({ queryKey }) => {
+const fetchMembers = async () => {
 	try {
 		const data = await fetch(`${process.env.PUBLIC_URL}/DB/department.json`);
 		const json = await data.json();
-		return json.members[queryKey[1]];
+		return json.members;
 	} catch (err) {
 		throw err;
 	}
 };
 
-export const useMembersQuery = (num) => {
+export const useMembersQuery = () => {
 	//useQuery는 첫번째 쿼리키에 두번째 함수의 promise 반환값을 mapping하기 때문에
 	//만약 중복 요청이 들어갈때 쿼리키가 동일하면 절대 refetching을 하지 않음
 	//만약 동일 서버데이터에서 인수에 따라 다른 값을 refetching하게 하고 싶으면 인수로 전달된 값을
 	//쿼리키의 배열 두번째 인수로 넘기면 인수에 값이 다르게 들어갈때마다 배열의 쿼리키값이 달라지므로 refetching가능해짐
-	return useQuery(['fetchMembers', num], fetchMembers, {
+	return useQuery(['fetchMembers'], fetchMembers, {
 		refetchOnMount: false,
 		refetchOnWindowFocus: false,
 		cacheTime: 1000 * 10,
@@ -24,15 +24,3 @@ export const useMembersQuery = (num) => {
 		staleTime: 1000 * 10,
 	});
 };
-//비동기데이터 5가지
-//pending : 데이터 요청중인 상태
-//fresh : 최신데이터 상태 (refetching안하는 상태)
-//stale : 오래된 데이터 상태 (refetching을 필요로 하는 상태)
-//inactive : 현재 컴포넌트에서 해당 서버데이터를 쓰지 않는 상태
-
-//inactive로 변경되는 조건
-//서버데이터가 fresh, stale 상관없이 inactive상태가되면 그 때부터 cacheTime이 소비되면서 Garbage Collector에 의해서 데이터가 제거됨
-
-//staleTime은 서버데이터의 fresh -> stale로 바뀌는 시간을 설정
-//데이터가 fresh상태면 데이터 fetching 요청이 들어가서 refetching하는 것이 아닌 캐시에 등록된 값을 재활용
-//데이터가 stale상태면 데이터 요청시 다시 refetching 처리
